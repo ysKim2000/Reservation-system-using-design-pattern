88ca22bc746a7e1c0e50c6c300cc2f6aff4a5bae
Yunseo Kim <86720575+ysKim2000@users.noreply.github.com>
2022-05-17 00:18:27 +0900

    Update testAPI.js

--- testAPI.js
+++ testAPI.js
@@ -10,9 +10,9 @@ const REQUEST_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffi
 var queryParams = '?' + encodeURIComponent('key') + '=999bdc7e274c0a5e1557a0642d612aee'; // Service Key
 queryParams += '&' + encodeURIComponent('targetDt') + '=' + encodeURIComponent(today); // item의 갯수 
 queryParams += '&' + encodeURIComponent('itemPerPage') + '=' + encodeURIComponent('10'); // item의 갯수 
-queryParams += '&' + encodeURIComponent('multiMovieYn') + '=' + encodeURIComponent(''); // Y: 다양성 영화, N: 상업영화 (default: 전체)
+queryParams += '&' + encodeURIComponent('multiMovieYn') + '=' + encodeURIComponent('N'); // Y: 다양성 영화, N: 상업영화 (default: 전체)
 queryParams += '&' + encodeURIComponent('repNationCd') + '=' + encodeURIComponent(''); // K: 한국영화, F: 외국영화 (default: 전체)
-// queryParams += '&' + encodeURIComponent('wideAreaCd') + '=' + encodeURIComponent('20200315'); //
+// queryParams += '&' + encodeURIComponent('wideAreaCd') + '=' + encodeURIComponent(''); // 지역 Code
 
 request({
     url: REQUEST_URL + queryParams,
