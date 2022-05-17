5255c523463dbdadbee3ce00d01af05f4c05d301
Yunseo Kim <86720575+ysKim2000@users.noreply.github.com>
2022-05-17 09:18:35 +0900

    Update testAPI.js

--- testAPI.js
+++ testAPI.js
@@ -4,11 +4,11 @@ const request = require('request');
 const moment = require('moment');
 
 // 이거 나중에 커링함수로 표현
-const today = moment().subtract(1, 'days').format('YYYYMMDD'); // 하루 전 날
+const targetDate = moment().subtract(1, 'days').format('YYYYMMDD'); // 하루 전 날
 
 const REQUEST_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
 var queryParams = '?' + encodeURIComponent('key') + '=999bdc7e274c0a5e1557a0642d612aee'; // Service Key
-queryParams += '&' + encodeURIComponent('targetDt') + '=' + encodeURIComponent(today); // item의 갯수 
+queryParams += '&' + encodeURIComponent('targetDt') + '=' + encodeURIComponent(targetDate); // item의 갯수 
 queryParams += '&' + encodeURIComponent('itemPerPage') + '=' + encodeURIComponent('10'); // item의 갯수 
 queryParams += '&' + encodeURIComponent('multiMovieYn') + '=' + encodeURIComponent('N'); // Y: 다양성 영화, N: 상업영화 (default: 전체)
 queryParams += '&' + encodeURIComponent('repNationCd') + '=' + encodeURIComponent(''); // K: 한국영화, F: 외국영화 (default: 전체)
