8b7d08925978706baa2777093e13e24993da38bb
Yunseo Kim <86720575+ysKim2000@users.noreply.github.com>
2022-05-27 21:03:13 +0900

    밥 먹고 와서 함

--- test.js
+++ test.js
@@ -37,18 +37,58 @@ var Gallery = new Culture('testGalleryName', 'testGalleryDate', 'testGalleryPlac
 // 코스 선택(A, B, C, D, E)
 // 영화 선택-> 시간 선택-> 좌석 선택-> 결제
 
+// 전략 패턴
+var Strategy = (function () {
+    function Strategy() {
+        this.strategy = null;
+    }
+    Strategy.prototype.setStrategy = function (strategy) {
+        this.strategy = strategy;
+    };
+    Strategy.prototype.execute = function () {
+        this.strategy.execute();
+    };
+    return Strategy;
+})();
+
+var CourseA = (function () {
+    function CourseA() { }
+    CourseA.prototype.execute = function () {
+        console.log('A');
+    };
+    return CourseA;
+})();
+
+var CourseB = (function () {
+    function CourseB() { }
+    CourseB.prototype.execute = function () {
+        console.log('B');
+    };
+    return CourseB;
+})();
+
+var CourseC = (function () {
+    function CourseC() { }
+    CourseC.prototype.execute = function () {
+        console.log('C');
+    };
+    return CourseC;
+})();
+
+// 입력
 const input = () => new Promise(resolve => {
     const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
+        input: process.stdin,
+        output: process.stdout
     });
-  
-    rl.on('line', line => {
-      rl.close();
-      resolve(line);
-    });
-  });
 
+    rl.on('line', line => {
+        rl.close();
+        resolve(line);
+    });
+});
+
+// API 불러오기
 var getMovieApi = function (REQUEST_URL, queryParams) { // JSON 
     console.log("API 요청 중..");
     request({
@@ -67,10 +107,54 @@ var getMovieApi = function (REQUEST_URL, queryParams) { // JSON
     });
 }
 
-var selectCourse = function(){
-    console.log()
-}
+// 코스 고르기
+var selectCourse = async function () {
+    do {
+        var approval;
+        console.log("[Course]");
+        console.log("1. A Course[Movie, Musical, Gallery]");
+        console.log("2. B Course[Movie, Play, Opera]");
+        console.log("3. C Course[Movie, Musical, Opera]");
+
+        process.stdout.write('입력(1,2,3): ');
+        var course = await input();
+
+        var strat = new Strategy();
+        if (course == 1) {
+            strat.setStrategy(new CourseA()); // A strategy
+        }
+        else if (course == 2) {
+            strat.setStrategy(new CourseB()); // B strategy
+        }
+        else if (course == 3) {
+            strat.setStrategy(new CourseC()); // C strategy
+        }
+        else {
+            console.log("잘못 입력하셨습니다.\n");
+            approval = true;
+            continue;
+        }
+        console.log("\n확실합니까?")
+        process.stdout.write("입력(yes or no): ");
+        var check = await input();
+
+        if(check == 'yes' || check == 'y' || check == 'Yes' || check == 'Y'){
+            strat.execute(); // execute strategy
+            approval = false;
+        }
+        else if(check == 'no' || check == 'n' || check == 'No' || check == 'N'){
+            approval = true;
+            continue;
+        }
+        else{
+            console.log("잘못 입력하셨습니다.\n");
+        }
+
+    } while (approval);
+    console.log("종료");
+
+};
 
 var main = async function(){
-    console.log()
+    selectCourse();
 }
