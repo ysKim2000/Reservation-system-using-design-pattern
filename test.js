7ef9f0502e227e78cf6f1025bde10ee4507f4d44
dong1hyun <94106437+dong1hyun@users.noreply.github.com>
2022-06-01 14:00:39 +0900

    시간 정하는거 구현 조금 해봄

--- test.js
+++ test.js
@@ -15,23 +15,65 @@ const input = () => new Promise(resolve => {
     });
 });
 
-const CultureInform = {
-    First_startTime : 0, 
-    Second_startTime : 0,
-    Third_startTime : 0,
-    First_startTimeList : ['10 : 00', '12 : 00', '14 : 00'],
+function Culture() {}
+
+Culture.prototype = {
+    First_startTimeNum : 0, 
+    Second_startTimeNum : 0,
+    Third_startTimeNum : 0,
+    First_startTimeList : [8, 10, 12, 14, 16],
+    Second_startTimeList : [10, 12, 14, 16, 18],
+    Third_startTimeList : [12, 14, 16, 18, 20],
+    TimeList : [0, 0, 0],
     settingTime : function(){
         var check;
+        var approval;
+        var timeNum;
+        type = this.type;
+        function settingCourseTime(TimeList, courseNum){
+            console.log("<" + this.type + " timetable>");
+            for(let i = 0; i < TimeList.length; i++){
+                console.log(i + 1  + ".【" + TimeList[i] + " : 00】" + "~【" + (TimeList[i] + 1) + " : 00】");
+            }
+            console.log("Choose the time you want to watch the movie(input : 1 ~ " + TimeList.length + ")");
+            timeNum = input();
+            if(timeNum < 1 || timeNum > TimeList.length){
+                console.log("Wrong.\n");
+                return true;
+            }
+            else{
+                console.log("false");
+                TimeList[courseNum] = timeNum;
+                return false;
+            }
+        }
+        var a = 0;
         do{
-            console.log("set the " + this.type + " time");
+            a++;
+            if(a == 10) return;
+            console.log("set the " + this.type + " time\n");
             
-        }while(check);
-        
+            if(this.First_startTimeNum == 0){
+                check = settingCourseTime(this.First_startTimeList, 0);
+                if(check){
+                    continue;
+                }
+                console.log("dd");
+                approval = true;
+            }
+            else if(this.Second_startTimeNum == 0){
+                check = settingCourseTime(this.Second_startTimeList, 1);
+                if(check) continue;
+                approval = true;
+            }
+            else{
+                check = settingCourseTime(this.Third_startTimeNum, 2);
+                if(check) continue;
+                approval = false;
+            }
+        }while(approval);
     }
 };
-
-function Culture() {}
-Culture.prototype = CultureInform;
 
 function watchShow(){}
 function watchArt(){}
@@ -57,14 +99,14 @@ Opera.prototype = watchShow.prototype;
 Gallery.prototype = watchArt.prototype;
 Museum.prototype = watchArt.prototype;
 
-// new Movie('movie', '범죄도시2').settingTime();
+new Movie('movie', '범죄도시2').settingTime();
+
 
 watchShow.prototype.seats = function (row, column) {
     seats = Array.from(Array(row), () => Array(column));
     return seats;
 }
 
-new Movie('movie', '범죄도시2').settingTime()
 
 Movie.prototype.selectMovie = async function () {
     var movieNum;
